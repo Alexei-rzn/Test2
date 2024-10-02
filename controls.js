68f5cd1b8a3a3334a698114e00d03be23548fab8
Alexei-rzn <onlinepeople24@gmail.com>
2024-10-02 14:54:01 +0300

    Update controls.js

--- controls.js
+++ controls.js
@@ -1,20 +1,16 @@
-// События для кнопок "Перезапуск", "Ход назад" и "Удалить плитку"
-document.getElementById("restart").addEventListener("click", () => {
-    gameOverDisplay.classList.add("hidden");
-    initGame();
-});
-
-document.getElementById("undo-btn").addEventListener("click", () => {
-    undoMove();
-});
-
-document.getElementById("delete-btn").addEventListener("click", () => {
-    const x = parseInt(document.getElementById("delete-x").value, 10);
-    const y = parseInt(document.getElementById("delete-y").value, 10);
-    
-    if (x >= 0 && x <= 3 && y >= 0 && y <= 3) {
-        deleteTile(x, y);
-    } else {
-        alert("Введите корректные координаты X и Y (0-3)");
+document.addEventListener('keydown', function (event) {
+    switch (event.key) {
+        case 'ArrowUp':
+            move('up');
+            break;
+        case 'ArrowDown':
+            move('down');
+            break;
+        case 'ArrowLeft':
+            move('left');
+            break;
+        case 'ArrowRight':
+            move('right');
+            break;
     }
 });
