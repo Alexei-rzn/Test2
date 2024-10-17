70b2dfbd0f8b9f68da16b4b0dd97514b34fe66af
Alexei-rzn <onlinepeople24@gmail.com>
2024-10-17 15:59:53 +0300

    Update game.js

--- game.js
+++ game.js
@@ -246,6 +246,80 @@ class Game2048 {
 
         return { newColumn, moved, combined };
     }
+// Сенсорное управление
+
+let touchStartX = 0;
+
+let touchStartY = 0;
+
+
+
+gridContainer.addEventListener('touchstart', (event) => {
+
+    touchStartX = event.touches[0].clientX;
+
+    touchStartY = event.touches[0].clientY;
+
+});
+
+
+
+gridContainer.addEventListener('touchmove', (event) => {
+
+    event.preventDefault(); // предотвращаем прокрутку страницы
+
+});
+
+
+
+gridContainer.addEventListener('touchend', (event) => {
+
+    const touchEndX = event.changedTouches[0].clientX;
+
+    const touchEndY = event.changedTouches[0].clientY;
+
+
+
+    const deltaX = touchEndX - touchStartX;
+
+    const deltaY = touchEndY - touchStartY;
+
+
+
+    const absDeltaX = Math.abs(deltaX);
+
+    const absDeltaY = Math.abs(deltaY);
+
+
+
+    if (absDeltaX > absDeltaY && absDeltaX > 30) {
+
+        if (deltaX > 0) {
+
+            move('right');
+
+        } else {
+
+            move('left');
+
+        }
+
+    } else if (absDeltaY > absDeltaX && absDeltaY > 30) {
+
+        if (deltaY > 0) {
+
+            move('down');
+
+        } else {
+
+            move('up');
+
+        }
+
+    }
+
+});
+
 
     saveState() {
         if (this.history.length >= 10) {
