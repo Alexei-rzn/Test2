b9a53298ea23f18e9ab6c21449e11bd5f01c4a3f
Alexei-rzn <onlinepeople24@gmail.com>
2024-10-18 14:36:38 +0300

    Update controls.js

--- controls.js
+++ controls.js
@@ -93,14 +93,17 @@ addFundsButton.addEventListener("click", () => {
 // Уровень сложности
 let currentDifficulty = 0;
 difficultyButton.addEventListener("click", () => {
-    currentDifficulty = (currentDifficulty + 1) % 5; // Циклический переход по уровням
-    difficultyButton.innerText = currentDifficulty + 1; // Обновляем текст кнопки
-    switch (currentDifficulty) {
-        case 0: game.tileProbability = [90, 10]; break;
-        case 1: game.tileProbability = [80, 20]; break;
-        case 2: game.tileProbability = [70, 30]; break;
-        case 3: game.tileProbability = [60, 40]; break;
-        case 4: game.tileProbability = [50, 50]; break;
+    if (game.canChangeDifficulty) {
+        currentDifficulty = (currentDifficulty + 1) % 5; // Циклический переход по уровням
+        difficultyButton.innerText = currentDifficulty + 1; // Обновляем текст кнопки
+        switch (currentDifficulty) {
+            case 0: game.tileProbability = [90, 10]; break;
+            case 1: game.tileProbability = [80, 20]; break;
+            case 2: game.tileProbability = [70, 30]; break;
+            case 3: game.tileProbability = [60, 40]; break;
+            case 4: game.tileProbability = [50, 50]; break;
+        }
+        game.canChangeDifficulty = false; // Запрет на изменение сложности во время игры
     }
 });
 
@@ -132,4 +135,17 @@ soundButton.addEventListener("click", () => {
 // Переход на страницу с таблицей лидеров
 ratingButton.addEventListener("click", () => {
     window.location.href = "victory.html"; // Переход на страницу таблицы лидеров
+});
+
+// Сохранение результата в таблицу лидеров
+submitScoreButton.addEventListener("click", () => {
+    const name = game.playerNameInput.value.trim();
+    if (name) {
+        game.saveToLeaderboard(name, difficultyButton.innerText); // Сохраняем результат
+        game.playerNameInput.value = ''; // Очищаем поле ввода
+        gameOverDisplay.classList.add("hidden"); // Скрываем окно окончания игры
+        game.initGame(); // Перезагрузка игры
+    } else {
+        alert("Пожалуйста, введите ваше имя!");
+    }
 });
